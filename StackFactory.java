9e3408ddb1b5178d6bf1e65735d791928aa600f6
camsandvl <san24358@uvg.edu.gt>
2025-02-27 17:39:26 -0600

    Update StackFactory.java

--- StackFactory.java
+++ StackFactory.java
@@ -1,1 +1,14 @@
-
+public class StackFactory {
+    public static <T> IStack<T> getStack(String tipo) {
+        switch (tipo.toLowerCase()) {
+            case "arraylist":
+                return new ArrayListStack<>();
+            case "vector":
+                return new VectorStack<>();
+            case "lista":
+                return new ListaStack<>();
+            default:
+                throw new IllegalArgumentException("Tipo de stack no válido");
+        }
+    }
+}
