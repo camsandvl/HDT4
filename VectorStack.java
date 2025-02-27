8ac8f7c796b7eaa37f6347f6a18e8cef019d647f
camsandvl <san24358@uvg.edu.gt>
2025-02-27 17:40:01 -0600

    Update VectorStack.java

--- VectorStack.java
+++ VectorStack.java
@@ -1,1 +1,28 @@
+import java.util.Vector;
+
+public class VectorStack<T> implements IStack<T> {
+    private Vector<T> stack = new Vector<>();
+
+    @Override
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) throw new RuntimeException("Stack vacío");
+        return stack.remove(stack.size() - 1);
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) throw new RuntimeException("Stack vacío");
+        return stack.get(stack.size() - 1);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
 
