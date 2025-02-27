da4aae652565dd2c2e244ad76be382246e1608e2
María Sierra <sie24405@uvg.edu.gt>
2025-02-27 12:07:11 -0600

    creación de instancia de calculadora para singleton

--- Calculadora.java
+++ Calculadora.java
@@ -1,1 +1,13 @@
+public class Calculadora {
+  private static Calculadora instance;
 
+  private Calculadora() {
+    public static Calculadora getInstance() {
+      if (instance == null) {
+        instance = new.Calculadora();
+      }
+      return instance
+    } 
+    
+  }
+}
