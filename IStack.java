b979a0c4a49210e9325a9f2e3a438f4e7847af8c
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-02-27 20:59:53 -0600

    Update IStack.java

--- IStack.java
+++ IStack.java
@@ -1,7 +1,12 @@
+/**
+    Emily Góngora, Camila Sandoval, Ale Sierra
+    Interfaz q define los métodos que debe tener un stack
+*/
 public interface IStack<T> {
     void push(T item);
     T pop();
     T peek();
     boolean isEmpty();
+    int size();
 }
 
