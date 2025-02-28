3d3bd9b12ec2418b277e584f577fa2cbaba9f950
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-02-27 21:01:07 -0600

    Update VectorStack.java

--- VectorStack.java
+++ VectorStack.java
@@ -1,3 +1,9 @@
+/**
+ * Emily Góngora, Camila Sandoval, Ale Sierra
+ * 
+ * Ejecuta el stack de tipo vector y implementa la interfaz IStack
+ * 
+ */
 import java.util.Vector;
 
 public class VectorStack<T> implements IStack<T> {
@@ -7,7 +13,10 @@ public class VectorStack<T> implements IStack<T> {
     public void push(T item) {
         stack.add(item);
     }
-
+    /**
+     * se usa un runtime exception para verificar si el stack esta vacio, en este caso, y verifica excepciones 
+     * que pueden ocurrir al ejectuar
+     */
     @Override
     public T pop() {
         if (isEmpty()) throw new RuntimeException("Stack vacío");
@@ -24,5 +33,9 @@ public class VectorStack<T> implements IStack<T> {
     public boolean isEmpty() {
         return stack.isEmpty();
     }
-}
 
+    @Override
+    public int size() {
+        return stack.size();
+    }
+}
